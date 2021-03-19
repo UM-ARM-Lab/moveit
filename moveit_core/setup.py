4a75ab6c8e6b63e97efff9c7eb155fdf1c558373
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2021-03-19 02:58:31 +0100

    Restructure module hierarchy into moveit.core

--- moveit_core/setup.py
+++ moveit_core/setup.py
@@ -1,11 +1,11 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+# ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
 from catkin_pkg.python_setup import generate_distutils_setup
-from setuptools import find_packages, setup
+from setuptools import setup
 
 d = generate_distutils_setup(
-    packages=find_packages('src'),
-    package_dir={'': 'src'}
+    packages=["moveit"],
+    package_dir={"": "python/src"}
 )
 
 setup(**d)
