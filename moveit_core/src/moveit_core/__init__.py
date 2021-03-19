7f65ab65509f3c2d90f98ae5c05447e8e8dac104
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2021-03-19 02:03:31 +0100

    fixup! Fix loading of submodules

--- moveit_core/src/moveit_core/__init__.py
+++ moveit_core/src/moveit_core/__init__.py
@@ -1,1 +1,5 @@
+# load symbols from C++ extension lib
+from pymoveit_core import *
+
+# and augment with symbols from python modules (order is important to allow overriding!)
 from . import collision_detection, kinematic_constraints, planning_scene, robot_model, robot_state, transforms
